867e74b1a59f139ec4fa5d8185f0fde8d04414e1
JaelsonJonas <rm93915@fiap.com.br>
2023-05-08 23:49:38 -0300

    CalculoIMC

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [peso, setPeso] = useState();
+  const [altura, setAltura] = useState();
+  const [resultado, setResultado] = useState();
+
+  const calculaIMC = () => {
+    let n1 = parseFloat(peso)
+    let n2 = parseFloat(altura)
+
+    let imc = n1 / (n2 * n2)
+
+    if (imc < 18.5) {
+      setResultado("Abaixo do peso")
+    } else if (imc >= 18.5 && imc < 25) {
+      setResultado("Peso Normal")
+    } else if (imc >= 25 && imc < 30) {
+      setResultado("Acima do peso")
+    } else {
+      setResultado("Obesidade")
+    }
+    return resultado
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Peso</Text>
+      <TextInput style={styles.input} value={peso} onChangeText={peso => setPeso(peso)}></TextInput>
+      <Text>Altura</Text>
+      <TextInput style={styles.input} value={altura} onChangeText={altura => setAltura(altura)}></TextInput>
+      <Button title='Calcular' onPress={() => calculaIMC()} />
+      <Text>Seu IMC esta no estagio: {resultado}</Text>
     </View>
   );
 }
@@ -13,8 +40,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    backgroundColor: '#F00',
+    color: '#FFF',
+    borderRadius: '1px',
+  },
+  button: {
+    fontSize: '15px'
+  }
 });
+
+export default App
